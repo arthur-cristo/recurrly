d32719ed13ada702bfe57f4d3d1f586e081c9674
Arthur Cristo <arthur.cristo.dev@gmail.com>
2026-05-17 17:04:47 -0300

    feat: routes

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,46 @@
+import { Text, View } from "react-native";
+import "@/global.css";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome!
+      </Text>
+      <Link
+        href="/onboarding"
+        className="mt-4 rounded bg-primary bg-primary text-white p-4"
+      >
+        Go to onboarding
+      </Link>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 rounded bg-primary bg-primary text-white p-4"
+      >
+        Go to Sign In
+      </Link>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 rounded bg-primary bg-primary text-white p-4"
+      >
+        Go to Sign Up
+      </Link>
+      <Link
+        href="/subscriptions/spotify"
+        className="mt-4 rounded bg-primary bg-primary text-white p-4"
+      >
+        Spotify Subscription
+      </Link>
+      <Link
+        href={{
+          pathname: "/subscriptions/[id]",
+          params: { id: "claude" },
+        }}
+        className="mt-4 rounded bg-primary bg-primary text-white p-4"
+      >
+        Claude Subscription
+      </Link>
+    </View>
+  );
+}
