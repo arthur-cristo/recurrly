135072e0d0d2f65b24aa85e6d22a22829038c100
Arthur Cristo <arthur.cristo.dev@gmail.com>
2026-05-17 17:41:59 -0300

    feat: create tabs based navigation

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -6,7 +6,7 @@ const SignIn = () => {
   return (
     <View>
       <Text>SignIn</Text>
-      <Link href="/">Create Account</Link>
+      <Link href="/(auth)/sign-up">Create Account</Link>
     </View>
   );
 };
